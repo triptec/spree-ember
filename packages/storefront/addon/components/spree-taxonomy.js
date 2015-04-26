a4cb291ee93c9a8e81e5718a0e6f1db5d9e5b761
Andreas Franzén <andreas@devil.se>
2015-04-26 10:00:33 +0200

    FIxed syntax and indentation

--- packages/storefront/addon/components/spree-taxonomy.js
+++ packages/storefront/addon/components/spree-taxonomy.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 import layout from '../templates/components/spree-taxonomy';
 export default Ember.Component.extend({
   layout: layout,
-  childTaxons: function(){
+  childTaxons: Ember.computed('taxonomy.taxons', function(){
     var taxons = this.get('taxonomy.taxons');
     var parentTaxon = taxons.findBy('parentId', null);
     var childTaxons = taxons.filter(function(taxon){
       return taxon.get('parentId') === parentTaxon.get('id');
     });
     return childTaxons;
-  }.property('taxonomy.taxons')
+  })
 });
